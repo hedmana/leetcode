3a7bd8a8490a3c336aa9b413e1436cefdd1cd999
Axel Hedman <axelhedman00@gmail.com>
2024-09-13 20:20:40 +0200

    LeetCode2

--- LeetCode2.cpp
+++ LeetCode2.cpp
@@ -10,20 +10,31 @@ namespace {
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {   
-        int longest = 0;
-
-        for(int i = 0; i < s.size(); i++) {
-            vector<bool> visited(256, false);
-
-            for (int j = i; j < s.size(); j++) {
-                if (visited[s[j]] == true) {
-                    break;
-                } else {
-                    longest = max(longest, j - i + 1);
-                    visited[s[j]] = true;
-                }
-            }
+        if (s.length() == 0) {
+            return 0;
         }
+
+        if (s.length() == 1) {
+            return 1; 
+        } 
+
+        int longest = 0; 
+        vector<bool> visited(256, false);
+
+        // sliding window pointers
+        int left = 0, right = 0;
+        while (right < s.length()) {
+
+            while (visited[s[right]] == true) {
+                visited[s[left]] = false;
+                left++;
+            }
+
+            visited[s[right]] = true;
+            longest = max(longest, (right - left + 1));
+            right++;
+        }
+        
         return longest;
     }
 };
@@ -33,6 +44,6 @@ int main() {
     string s = "abcabcbb";
 
     cout << "Result: " << solution.lengthOfLongestSubstring(s) << endl;
-        
+
     return 0;
 }
