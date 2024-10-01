8924f3691e258b316d7edd5e7430b80722da2b6f
Axel Hedman <axelhedman00@gmail.com>
2024-10-01 11:00:56 +0200

    LeetCode 19 & 141

---
+++ LeetCode141.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if (!head) { return false; }
+        ListNode *slow = head;
+        ListNode *fast = head->next;
+        while (slow != fast) {
+            if (!fast || !fast->next) {
+                return false;
+            }
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return true; 
+    }
+};
+
+int main() {
+    Solution sol;
+    ListNode *head = new ListNode(3);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(0);
+    head->next->next->next = new ListNode(-4);
+    cout << sol.hasCycle(head) << endl; 
+    return 0;  
+}
