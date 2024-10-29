a1b2dcffa0c38ec59f7de049235de90790667c5c
Axel Hedman <axelhedman00@gmail.com>
2024-10-29 19:29:30 +0100

    LeetCode80

---
+++ LeetCode80.cpp
@@ -0,0 +1,31 @@
+#include  <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+    int removeDuplicates(vector<int>& nums) {
+       int n = nums.size();
+
+        if (n <= 2) {
+            return n;
+        }
+
+        int slow = 2, fast = 2;
+        while (fast < n) {
+            if (nums[slow - 2] != nums[fast]) {
+                nums[slow] = nums[fast];
+                ++slow;
+            }
+            ++fast;
+        }
+
+        return slow;
+    }
+};
+
+int main() {
+    cout << "Hello, World!" << endl;
+    return 0;
+}
+
