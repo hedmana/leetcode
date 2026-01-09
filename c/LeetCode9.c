866da302c1c81dc801af56f5c2122059404bb867
Axel Hedman <axel.hedman@gostalabs.com>
2026-01-09 22:37:33 +0200

    LeetCode9

---
+++ c/LeetCode9.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+bool isPalindrome(int x) {
+    if (x < 0) return false;
+    if (x % 10 == 0 && x != 0) return false;
+
+    int rev = 0;
+    while (x > rev) {
+        rev = rev * 10 + (x % 10);
+        x /= 10;
+    }
+
+    return (x == rev) || (x == rev / 10);
+}   
+
+int main() {
+    int x, ans; 
+
+    x = 1001;
+
+    ans = isPalindrome(x);
+
+    printf("Test: %d", ans);
+
+    return 0;
+}
