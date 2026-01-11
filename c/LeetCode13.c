0dd7d26064605d156a0206a3032719b76465d8e1
Axel Hedman <axel.hedman@gostalabs.com>
2026-01-11 16:30:22 +0200

    LeetCode13

---
+++ c/LeetCode13.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int romanVal(char c) {
+    switch (c) {
+        case 'I': return 1;
+        case 'V': return 5;
+        case 'X': return 10;
+        case 'L': return 50;
+        case 'C': return 100;
+        case 'D': return 500;
+        case 'M': return 1000;
+        default: return 0;
+    }
+} 
+
+int romanToInt(char* s) {
+    int ans = 0;
+
+    for (int i = 0; s[i] != '\0'; i++) {
+        int cur = romanVal(s[i]);
+        int next = romanVal(s[i + 1]);
+        
+        if (cur < next) {
+            ans -= cur;
+        } else {
+            ans += cur;
+        }
+    }
+
+    return ans;
+}
+
+    int main() {
+    char s[] = "XII";
+
+    printf("%d", romanToInt(s));
+
+    return 0;
+}
